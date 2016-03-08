3e3af7b67862eca2583d02ab05403c640fc29e8f
Jorge Mendoza <jorgevmendoza@gmail.com>
2016-03-07 21:30:41 -0430

    First Commit

---
+++ index.js
@@ -0,0 +1,21 @@
+var express = require('express');
+var https = require('https');
+var app = express();
+
+var options = {
+	host: 'localhost',
+   	port: 80,
+   	path: '/info.php'
+}
+
+app.get('/', function(response, request){
+
+	https.get(options, function(res){
+		console.log('Response status', res.status);
+	});
+	response.send(res.status);
+});
+
+app.listen('3000', function(){
+	console.log('App is running');
+});
