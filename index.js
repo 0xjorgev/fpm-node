9ea3eacfc238c63774bf65fcb03ad850be38110d
Jorge Mendoza <jorgevmendoza@gmail.com>
2016-03-07 22:17:40 -0430

    added body to the response

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ app.get('/', function(request, response){
 	console.log('Getting GET Requests');
 
 	http.get(options, function(res){
-		console.log('Response status', res.headers);
+		console.log('Response status', res.body);
 		//response.send(res.status);
 	})
 
